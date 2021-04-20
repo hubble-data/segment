9dcbcb11739aae31c8839237e297ebb4b819a03c
Katie Hindson <hindsonk@gmail.com>
2021-04-20 15:57:16 +0200

    changes ts

--- models/base/base_segment_users.sql
+++ models/base/base_segment_users.sql
@@ -4,7 +4,7 @@ WITH source AS (
     id AS user_id,
     uuid_ts AS user_activated_ts,
     DATE(uuid_ts) AS user_activated_date,
-    ROW_NUMBER() OVER(PARTITION BY id ORDER BY timestamp ASC) AS rank
+    ROW_NUMBER() OVER(PARTITION BY id ORDER BY uuid_ts ASC) AS rank
 
     FROM {{var('segment_users_table')}}
 )
