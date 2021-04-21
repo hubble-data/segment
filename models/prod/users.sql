99d4cd53b14769e8143c30bdbb44d7c27fec7ac1
Katie Hindson <hindsonk@gmail.com>
2021-04-21 17:19:56 +0200

    adds docs

--- models/prod/users.sql
+++ models/prod/users.sql
@@ -5,7 +5,14 @@
 }}
 
 SELECT
-*
+user_id,
+user_activated_date,
+days_since_activated,
+num_web_sessions,
+is_7d_web_active AS is_7d_web_active_latest,
+num_web_sessions_prev_7d,
+num_web_sessions_prev_30d,
+num_web_sessions_cum AS num_web_sessions_total
 
 FROM {{ref('users_daily')}}
 
